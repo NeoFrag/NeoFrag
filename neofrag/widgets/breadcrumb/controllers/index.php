96afafa6178549924201b931f2db2caf7554f9c6
Michaël BILCOT <foxley@neofrag.fr>
2017-03-03 18:52:02 +0100

    [Library][Breadcrumb] Is a Library now

--- neofrag/widgets/breadcrumb/controllers/index.php
+++ neofrag/widgets/breadcrumb/controllers/index.php
@@ -22,7 +22,7 @@ class w_breadcrumb_c_index extends Controller_Widget
 {
 	public function index($config = [])
 	{
-		$count = count($links = $this->breadcrumb->get_links());
+		$count = count($links = NeoFrag()->module->breadcrumb->get_links());
 		
 		array_walk($links, function(&$value, $key) use ($count){
 			$value = '<li'.(($is_last = $key == $count - 1) ? ' class="active"' : '').'><a href="'.url($value[1]).'">'.($is_last && $value[2] !== '' ? icon($value[2]).' ' : '').$value[0].'</a></li>';
