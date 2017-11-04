38a305de687cbe1bd8556d2ce5ceb3da220bc4ca
Michaël BILCOT <foxley@neofrag.fr>
2017-11-04 17:11:42 +0100

    [Library][Json] Adding new library

---
+++ neofrag/libraries/json.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * https://neofr.ag
+ * @author: Michaël BILCOT <michael.bilcot@neofr.ag>
+ */
+
+namespace NF\NeoFrag\Libraries;
+
+use NF\NeoFrag\Library;
+
+class Json extends Library
+{
+	protected $_output;
+
+	public function __invoke($output)
+	{
+		$this->_output = $output;
+		return $this;
+	}
+
+	public function __toString()
+	{
+		header('Content-Type: application/json; charset=UTF-8');
+
+		$output = $this->_output;
+
+		if ($notifications = $this->session('notifications'))
+		{
+			$output['notify'] = array_map(function($a){
+				$a['message'] = (string)$a['message'];
+				return $a;
+			}, $notifications);
+
+			$this->session->destroy('notifications');
+		}
+
+		if (($output = json_encode($output)) === FALSE)
+		{
+			$output = json_encode([
+				'error' => json_last_error_msg()
+			]);
+		}
+
+		return $output;
+	}
+}
