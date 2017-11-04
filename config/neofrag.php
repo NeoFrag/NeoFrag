d9adad0909b5aa5b58e8a9b436dd4664e574f3f6
Michaël BILCOT <foxley@neofrag.fr>
2017-11-04 20:26:08 +0100

    [NeoFrag] Adding global config file

---
+++ config/neofrag.php
@@ -0,0 +1,7 @@
+<?php
+
+define('NEOFRAG_DEBUG_BAR',  FALSE);
+define('NEOFRAG_SAFE_MODE',  FALSE);
+define('NEOFRAG_LOGS',       FALSE);
+define('NEOFRAG_LOGS_DB',    FALSE);
+define('NEOFRAG_LOGS_I18N',  FALSE);
--- index.php
+++ index.php
@@ -9,10 +9,16 @@ define('NEOFRAG_TIME',    microtime(TRUE));
 define('NEOFRAG_CMS',     __DIR__);
 define('NEOFRAG_VERSION', 'Alpha 0.1.6.1');
 
+error_reporting(E_ALL);
+
+ini_set('error_log',       'logs/php.log');
+ini_set('display_errors',  TRUE);
 ini_set('default_charset', 'UTF-8');
-ini_set('mbstring.func_overload', 7);
+
 mb_regex_encoding('UTF-8');
 mb_internal_encoding('UTF-8');
+
+require_once 'config/neofrag.php';
 
 function class_name($name)
 {
