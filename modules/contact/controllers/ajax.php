548f25ffdb879f472a6469b3f2a0777bb08dfca2
Michaël BILCOT <foxley@neofrag.fr>
2017-11-06 16:25:17 +0100

    [Module][Contact] Improvement with ajax modal

--- modules/contact/controllers/ajax.php
+++ modules/contact/controllers/ajax.php
@@ -8,11 +8,12 @@ namespace NF\Modules\Contact\Controllers;
 
 use NF\NeoFrag\Loadables\Controllers\Module as Controller_Module;
 
-class Index extends Controller_Module
+class Ajax extends Controller_Module
 {
 	public function index()
 	{
 		return $this->form2('contact')
-					->panel();
+					->modal($this->lang('Nous contacter'), 'fa-envelope-o')
+					->cancel();
 	}
 }
---
+++ modules/contact/forms/contact.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * https://neofr.ag
+ * @author: Michaël BILCOT <michael.bilcot@neofr.ag>
+ */
+
+$this->rule($this->form_text('subject')
+				->title('Votre objet')
+				->required())
+	->rule_if(!$this->user->email, $this->form_email('email')
+										->title('Votre adresse email')
+										->required())
+	->rule($this->form_textarea('message')
+				->title('Votre message')
+				->required())
+	->captcha()
+	->submit('Envoyer')
+	->success(function($data, $form){
+		$sent = $this	->anti_flood()
+						->email
+						->from($this->user->email ?: $data['email'])
+						->to($this->config->nf_contact)
+						->subject($data['subject'])
+						->message(function() use ($data){
+							return [
+								'content' => nl2br(strtolink($data['message'])).($this->user() ? '<br /><br />'.$this->user->view('user/profile') : '')
+							];
+						})
+						->send();
+
+		if ($sent)
+		{
+			notify('Message envoyé');
+			$this->modal->dispose();
+		}
+		else
+		{
+			$form->error('Une erreur s\'est produite lors de l\'envoi du message');
+		}
+	});
