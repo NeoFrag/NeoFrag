eab08b4d9b0d74e15a3a5fa727d25f4395c28047
Michaël BILCOT <foxley@neofrag.fr>
2017-11-06 16:48:53 +0100

    [Library][Modal] Adding param to add modal-body or not

--- neofrag/libraries/modal.php
+++ neofrag/libraries/modal.php
@@ -13,6 +13,7 @@ class Modal extends Library
 	protected $_header;
 	protected $_buttons = [];
 	protected $_body;
+	protected $_body_tags;
 	protected $_size;
 	protected $_form;
 
@@ -27,11 +28,18 @@ class Modal extends Library
 
 	public function __toString()
 	{
+		$content = '';
+
+		if ($this->_body)
+		{
+			$content .= $this->_body_tags ? '<div class="modal-body">'.$this->_body.'</div>' : $this->_body;
+		}
+
 		$content = '<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="'.$this->lang('close').'"><span aria-hidden="true">&times;</span></button>
 						<h4 class="modal-title">'.$this->_header.'</h4>
 					</div>
-					<div class="modal-body">'.$this->_body.'</div>
+					'.$content.'
 					'.($this->_buttons ? $this->button->static_footer($this->_buttons, 'right')->append_attr('class', 'modal-footer') : '');
 
 		if ($this->_form)
@@ -53,9 +61,10 @@ class Modal extends Library
 				</div>';
 	}
 
-	public function body($body)
+	public function body($body, $add_body_tags = TRUE)
 	{
-		$this->_body = $body;
+		$this->_body      = $body;
+		$this->_body_tags = $add_body_tags;
 		return $this;
 	}
 
