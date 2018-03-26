14e2bc295709d980b2af621fd29031298d67c3fd
Michaël BILCOT <foxley@neofrag.fr>
2018-03-26 17:46:30 +0200

    [Authenticator][Discord] New authenticator

---
+++ addons/authenticator_discord/authenticator_discord.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * https://neofr.ag
+ * @author: Michaël BILCOT <michael.bilcot@neofr.ag>
+ */
+
+namespace NF\Addons\Authenticator_Discord;
+
+use NF\NeoFrag\Addons\Authenticator;
+
+class Authenticator_Discord extends Authenticator
+{
+	protected function __info()
+	{
+		return [
+			'title'   => 'Discord',
+			'icon'    => 'fa-commenting-o',
+			'color'   => '#7289DA',
+			'help'    => 'https://discordapp.com/developers/applications/me#top',
+			'version' => '1.0',
+			'depends' => [
+				'addon/authenticator' => '1.0'
+			]
+		];
+	}
+
+	public function config()
+	{
+		return array_merge(parent::config(), [
+			'scope' => ['identify']
+		]);
+	}
+
+	public function data(&$params = [])
+	{
+		if (!empty($_GET['code']) && !empty($_GET['state']))
+		{
+			$params = $_GET;
+
+			return function($data){
+				return [
+					'id'       => $data->id,
+					'username' => $data->username,
+					'avatar'   => 'https://cdn.discordapp.com/avatars/'.$data->id.'/'.$data->avatar.'.png?size=512'
+				];
+			};
+		}
+	}
+}
