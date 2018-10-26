9a6605ea80ea847d30009a80d50ea1d1327fc7b1
Jérémy VALENTIN <eresnova@neofrag.fr>
2018-10-26 20:13:22 +0200

    [Module][Partners] Some improvements

--- modules/partners/views/index.tpl.php
+++ modules/partners/views/index.tpl.php
@@ -2,22 +2,22 @@
 $count = count($partners);
 foreach ($partners as $i => $partner): ?>
 	<div class="row">
-		<div class="col-5 text-center" style="padding-right: 15px;">
+		<div class="col-4 text-center">
 			<a href="<?php echo url('partners/'.$partner['partner_id'].'/'.$partner['name']) ?>" target="_blank" class="thumbnail" style="padding: 10px;">
 				<?php if ($partner[$this->config->partners_logo_display]): ?>
 				<img src="<?php echo NeoFrag()->model2('file', $partner[$this->config->partners_logo_display])->path() ?>" class="img-fluid" alt="" />
 				<?php else: ?>
-				<h3 class="m-0"><?php echo $partner['title'] ?></h3>
+				<h3><?php echo $partner['title'] ?></h3>
 				<?php endif ?>
 			</a>
 		</div>
-		<div class="col-7">
-			<div class="pull-right"><a class="text-muted" href="<?php echo url('partners/'.$partner['partner_id'].'/'.$partner['name']) ?>" target="_blank"><?php echo preg_replace('_https?://_', '', $partner['website']) ?></a></div>
-			<h4>À propos de <b><?php echo $partner['title'] ?></b></h4>
+		<div class="col-8">
+			<h4>À propos de <?php echo $partner['title'] ?></h4>
+			<p><?php echo $this->lang('Site internet') ?> : <a href="<?php echo url('partners/'.$partner['partner_id'].'/'.$partner['name']) ?>" target="_blank"><?php echo preg_replace('_https?://_', '', $partner['website']) ?></a></p>
 			<ul class="list-inline">
-				<?php if ($partner['facebook']) echo '<li><a href="'.$partner['facebook'].'" class="btn-primary" target="_blank" data-toggle="tooltip" title="Facebook">'.icon('fa-facebook').'</a></li>' ?>
-				<?php if ($partner['twitter']) echo '<li><a href="'.$partner['twitter'].'" class="btn-info" target="_blank" data-toggle="tooltip" title="Twitter">'.icon('fa-twitter').'</a></li>' ?>
-				<?php if ($partner['code']) echo '<li><span class="btn-link" data-toggle="tooltip" title="Code promotionnel">'.icon('fa-gift').'</span> '.$partner['code'].'</li>' ?>
+				<?php if ($partner['facebook']) echo '<li class="list-inline-item"><a href="'.$partner['facebook'].'" class="btn btn-primary" target="_blank" data-toggle="tooltip" title="Facebook">'.icon('fa-facebook').'</a></li>' ?>
+				<?php if ($partner['twitter']) echo '<li class="list-inline-item"><a href="'.$partner['twitter'].'" class="btn btn-info" target="_blank" data-toggle="tooltip" title="Twitter">'.icon('fa-twitter').'</a></li>' ?>
+				<?php if ($partner['code']) echo '<li class="list-inline-item"><span data-toggle="tooltip" title="Code promotionnel">'.icon('fa-gift').' '.$partner['code'].'</span></li>' ?>
 			</ul>
 			<?php if ($partner['description']) echo '<p>'.bbcode($partner['description']).'</p>' ?>
 		</div>
