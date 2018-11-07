9ded6cd844b6023936a0c33fa37f71179225078b
Jérémy VALENTIN <eresnova@neofrag.fr>
2018-11-07 20:15:21 +0100

    [Widget][Forum] Some improvements

--- widgets/forum/views/index.tpl.php
+++ widgets/forum/views/index.tpl.php
@@ -1,9 +1,11 @@
 <?php foreach ($messages as $message): ?>
 <div class="media">
-	<?php echo NeoFrag()->model2('user', $message['user_id'])->avatar() ?>
+	<div class="media-left">
+		<?php echo NeoFrag()->model2('user', $message['user_id'])->avatar() ?>
+	</div>
 	<div class="media-body">
-		<p><a href="<?php echo url('forum/topic/'.$message['topic_id'].'/'.url_title($message['topic_title'])) ?>#<?php echo $message['message_id'] ?>" data-toggle="tooltip" title="<?php echo $message['topic_title'] ?>"><?php echo str_shortener(strip_tags(str_replace('<br />', ' ', bbcode($message['message']))), 150) ?></a></p>
-		<?php echo icon('fa-clock-o').' '.time_span($message['date']) ?>
+		<a href="<?php echo url('forum/topic/'.$message['topic_id'].'/'.url_title($message['topic_title'])) ?>#<?php echo $message['message_id'] ?>" data-toggle="tooltip" title="<?php echo $message['topic_title'] ?>"><?php echo str_shortener(strip_tags(str_replace('<br />', ' ', bbcode($message['message']))), 150) ?></a><br />
+		<small><?php echo icon('fa-clock-o').' '.time_span($message['date']) ?></small>
 	</div>
 </div>
 <?php endforeach ?>
--- widgets/forum/views/topics.tpl.php
+++ widgets/forum/views/topics.tpl.php
@@ -1,9 +1,11 @@
 <?php foreach ($topics as $topic): ?>
 <div class="media">
-	<?php echo NeoFrag()->model2('user', $topic['user_id'])->avatar() ?>
+	<div class="media-left">
+		<?php echo NeoFrag()->model2('user', $topic['user_id'])->avatar() ?>
+	</div>
 	<div class="media-body">
-		<p><a href="<?php echo url('forum/topic/'.$topic['topic_id'].'/'.url_title($topic['title'])) ?>"><?php echo $topic['title'] ?></a></p>
-		<?php echo icon('fa-clock-o').' '.time_span($topic['date']).' '.icon('fa-comments-o').' '.$topic['count_messages'] ?>
+		<a href="<?php echo url('forum/topic/'.$topic['topic_id'].'/'.url_title($topic['title'])) ?>"><?php echo $topic['title'] ?></a><br />
+		<small><?php echo icon('fa-clock-o').' '.time_span($topic['date']).' '.icon('fa-comments-o').' '.$topic['count_messages'] ?></small>
 	</div>
 </div>
 <?php endforeach ?>
