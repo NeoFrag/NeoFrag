7edcee298fdd10b7602470057a95d926a507c095
Jérémy VALENTIN <eresnova@neofrag.fr>
2018-11-13 15:57:10 +0100

    [Widget][Partners] Fix carousel display

--- widgets/partners/views/index.tpl.php
+++ widgets/partners/views/index.tpl.php
@@ -21,43 +21,45 @@
 	width: 40px;
 }
 </style>
-<div id="partners-carousel-<?php echo $id ?>" class="carousel slide" data-ride="carousel">
-	<div class="carousel-inner" role="listbox">
-		<?php
-		$i = 0;
-		for ($slider = 1; $slider <= $total_slides; $slider++): ?>
-		<div class="carousel-item<?php if ($slider == 1) echo ' active' ?>">
-			<div class="row">
-				<?php
-				$max = $slider == $total_slides ? $total_partners - (($slider - 1) * $display_number) : $display_number;
-				for ($partner = 1; $partner <= $max; $partner++)
-				{
-				?>
-				<div class="col-<?php echo ceil(12 / $display_number) ?> partner-item">
-					<a href="<?php echo url('partners/'.$partners[$i]['partner_id'].'/'.$partners[$i]['name']) ?>" target="_blank">
-						<?php if ($image_id = $partners[$i]['logo_'.$display_style]): ?>
-						<img class="logo" src="<?php echo NeoFrag()->model2('file', $image_id)->path() ?>" alt="" />
-						<?php else: ?>
-						<?php echo $partners[$i]['title'] ?>
-						<?php endif ?>
-					</a>
+<div class="p-3">
+	<div id="partners-carousel-<?php echo $id ?>" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner" role="listbox">
+			<?php
+			$i = 0;
+			for ($slider = 1; $slider <= $total_slides; $slider++): ?>
+			<div class="carousel-item<?php if ($slider == 1) echo ' active' ?>">
+				<div class="row">
+					<?php
+					$max = $slider == $total_slides ? $total_partners - (($slider - 1) * $display_number) : $display_number;
+					for ($partner = 1; $partner <= $max; $partner++)
+					{
+					?>
+					<div class="col-<?php echo ceil(12 / $display_number) ?> partner-item">
+						<a href="<?php echo url('partners/'.$partners[$i]['partner_id'].'/'.$partners[$i]['name']) ?>" target="_blank">
+							<?php if ($image_id = $partners[$i]['logo_'.$display_style]): ?>
+							<img class="logo" src="<?php echo NeoFrag()->model2('file', $image_id)->path() ?>" alt="" />
+							<?php else: ?>
+							<?php echo $partners[$i]['title'] ?>
+							<?php endif ?>
+						</a>
+					</div>
+					<?php
+						$i++;
+					}
+					?>
 				</div>
-				<?php
-					$i++;
-				}
-				?>
 			</div>
+			<?php endfor ?>
 		</div>
-		<?php endfor ?>
+		<?php if ($total_slides > 1): ?>
+		<a class="carousel-control-prev" href="#partners-carousel-<?php echo $id ?>" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#partners-carousel-<?php echo $id ?>" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+		<?php endif ?>
 	</div>
-	<?php if ($total_slides > 1): ?>
-	<a class="carousel-control-prev" href="#partners-carousel-<?php echo $id ?>" role="button" data-slide="prev">
-		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		<span class="sr-only">Previous</span>
-	</a>
-	<a class="carousel-control-next" href="#partners-carousel-<?php echo $id ?>" role="button" data-slide="next">
-		<span class="carousel-control-next-icon" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-	</a>
-	<?php endif ?>
 </div>
