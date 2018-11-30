RebornSpartan/neofrag-cms

[Model][File] Library dropped and replaced by file model

modules/awards/views/award.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($image_id): ?>
-	<img class="img-responsive" src="<?php echo path($image_id) ?>" alt="" />
+	<img class="img-responsive" src="<?php echo NeoFrag()->model2('file', $image_id)->path() ?>" alt="" />
 <?php endif ?>
 <div class="panel-body">
 	<div class="well text-center">

modules/events/views/event.tpl.php
@@ -1,11 +1,11 @@
-<a href="<?php echo $link = url('events/'.$event_id.'/'.url_title($title)) ?>"><img src="<?php echo path($image_id) ?>" class="img-responsive" alt="" /></a>
+<a href="<?php echo $link = url('events/'.$event_id.'/'.url_title($title)) ?>"><img src="<?php echo NeoFrag()->model2('file', $image_id)->path() ?>" class="img-responsive" alt="" /></a>
 <?php if (!empty($match['opponent']))://Matches ?>
 <div class="panel-body text-center">
 	<div class="row vcenter">
 		<div class="text-right col-xs-5 vcenter">
 			<h3 class="no-margin">
 				<a href="<?php echo url('events/team/'.$match['team_id'].'/'.$match['team']['name']) ?>">
-				<?php if ($icon = path($match['team']['icon_id'])) echo '<img src="'.path($icon).'" style="margin-right: 10px;" alt="" />' ?>
+				<?php if ($icon = NeoFrag()->model2('file', $match['team']['icon_id'])->path()) echo '<img src="'.NeoFrag()->model2('file', $icon)->path().'" style="margin-right: 10px;" alt="" />' ?>
 				<?php echo $match['team']['title'].' '.$this->model('matches')->display_scores($match['scores'], $color) ?>
 				</a>
 			</h3>
@@ -21,7 +21,7 @@
 		<?php endif ?>
 		<?php if ($match['opponent']['image_id']): ?>
 		<div class="text-right col-xs-1 vcenter">
-			<img src="<?php echo path($match['opponent']['image_id']) ?>" class="img-responsive" alt="" />
+			<img src="<?php echo NeoFrag()->model2('file', $match['opponent']['image_id'])->path() ?>" class="img-responsive" alt="" />
 		</div>
 		<?php endif ?>
 		<div class="text-left col-xs-<?php echo $match['opponent']['image_id'] ? 4 : 5 ?> vcenter">
@@ -58,7 +58,7 @@
 			<div class="row vcenter">
 				<?php if ($rounds[$i]['image_id']): ?>
 				<div class="col-xs-2 vcenter">
-					<img src="<?php echo path($rounds[$i]['image_id']) ?>" class="img-responsive" alt="" />
+					<img src="<?php echo NeoFrag()->model2('file', $rounds[$i]['image_id'])->path() ?>" class="img-responsive" alt="" />
 				</div>
 				<?php endif ?>
 				<div class="col-xs-<?php echo $rounds[$i]['image_id'] ? '3' : '5' ?> vcenter">

modules/gallery/views/index.tpl.php
@@ -1,5 +1,5 @@
 <?php if (!empty($category_image)): ?>
-<img class="img-responsive" src="<?php echo path($category_image) ?>" alt="" />
+<img class="img-responsive" src="<?php echo NeoFrag()->model2('file', $category_image)->path() ?>" alt="" />
 <?php endif ?>
 <div class="panel-body">
 	<?php if (!empty($gallery)): ?>
@@ -7,7 +7,7 @@
 		<?php foreach ($gallery as $album): ?>
 			<div class="col-md-12">
 				<h4><a href="<?php echo url('gallery/album/'.$album['gallery_id'].'/'.url_title($album['title'])) ?>"><?php echo $album['title'] ?></a></h4>
-				<a href="<?php echo url('gallery/album/'.$album['gallery_id'].'/'.url_title($album['title'])) ?>" class="gallery-item thumbnail" style="background-image: url('<?php echo path(!empty($album['image']) ? $album['image'] : $this->db->select('file_id')->from('nf_gallery_images')->where('gallery_id', $album['gallery_id'])->order_by('RAND()')->limit(1)->row()) ?>');">
+				<a href="<?php echo url('gallery/album/'.$album['gallery_id'].'/'.url_title($album['title'])) ?>" class="gallery-item thumbnail" style="background-image: url('<?php echo NeoFrag()->model2('file', !empty($album['image']) ? $album['image'] : $this->db->select('file_id')->from('nf_gallery_images')->where('gallery_id', $album['gallery_id'])->order_by('RAND()')->limit(1)->row())->path() ?>);">
 					<div class="black-caption-hover">
 						<div class="black-caption-hover-content">
 							<h4 class="no-margin"><?php echo icon('fa-eye').' '.$this->lang('Ouvrir l\'album') ?></h4>

modules/teams/views/index.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($image_id): ?>
-<a href="<?php echo url('teams/'.$team_id.'/'.$name) ?>"><img class="img-responsive" src="<?php echo path($image_id) ?>" alt="" /></a>
+<a href="<?php echo url('teams/'.$team_id.'/'.$name) ?>"><img class="img-responsive" src="<?php echo NeoFrag()->model2('file', $image_id)->path() ?>" alt="" /></a>
 <?php endif ?>
 <div class="panel-body">
 	<?php if ($description): ?>

widgets/events/views/matches.tpl.php
@@ -8,7 +8,7 @@
 			</div>
 			<?php if ($match['match']['opponent']['image_id']): ?>
 			<div class="text-center col-md-1">
-				<img src="<?php echo path($match['match']['opponent']['image_id']) ?>" class="img-responsive" alt="" />
+				<img src="<?php echo NeoFrag()->model2('file', $match['match']['opponent']['image_id'])->path() ?>" class="img-responsive" alt="" />
 			</div>
 			<?php endif ?>
 			<div class="text-left col-md-<?php echo $match['match']['opponent']['image_id'] ? 7 : 8 ?>">

widgets/gallery/controllers/index.php
@@ -50,7 +50,7 @@ class Index extends Controller_Widget
 		{
 			return $this->panel()
 						->heading($image['title'])
-						->body('<a href="'.$href.'"><img class="img-responsive" src="'.path($image['file_id']).'" alt="" /></a>', FALSE)
+						->body('<a href="'.$href.'"><img class="img-responsive" src="'.NeoFrag()->model2('file', $image['file_id'])->path().'" alt="" /></a>', FALSE)
 						->footer('<a href="'.$href.'">'.icon('fa-arrow-circle-o-right').' '.$this->lang('Détails').'</a>', 'right');
 		}
 		else

NeoFrag/NeoFrag

modules/awards/views/award.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($image_id): ?>
-	<img class="img-fluid" src="<?php echo path($image_id) ?>" alt="" />
+	<img class="img-fluid" src="<?php echo NeoFrag()->model2('file', $image_id)->path() ?>" alt="" />
 <?php endif ?>
 <div class="panel-body">
 	<div class="well text-center">

modules/events/views/event.tpl.php
@@ -1,11 +1,11 @@
-<a href="<?php echo $link = url('events/'.$event_id.'/'.url_title($title)) ?>"><img src="<?php echo path($image_id) ?>" class="img-fluid" alt="" /></a>
+<a href="<?php echo $link = url('events/'.$event_id.'/'.url_title($title)) ?>"><img src="<?php echo NeoFrag()->model2('file', $image_id)->path() ?>" class="img-fluid" alt="" /></a>
 <?php if (!empty($match['opponent']))://Matches ?>
 <div class="panel-body text-center">
 	<div class="row vcenter">
 		<div class="text-right col-5 vcenter">
 			<h3 class="m-0">
 				<a href="<?php echo url('events/team/'.$match['team_id'].'/'.$match['team']['name']) ?>">
-				<?php if ($icon = path($match['team']['icon_id'])) echo '<img src="'.path($icon).'" style="margin-right: 10px;" alt="" />' ?>
+				<?php if ($icon = NeoFrag()->model2('file', $match['team']['icon_id'])->path()) echo '<img src="'.NeoFrag()->model2('file', $icon)->path().'" style="margin-right: 10px;" alt="" />' ?>
 				<?php echo $match['team']['title'].' '.$this->model('matches')->display_scores($match['scores'], $color) ?>
 				</a>
 			</h3>
@@ -21,7 +21,7 @@
 		<?php endif ?>
 		<?php if ($match['opponent']['image_id']): ?>
 		<div class="text-right col-1 vcenter">
-			<img src="<?php echo path($match['opponent']['image_id']) ?>" class="img-fluid" alt="" />
+			<img src="<?php echo NeoFrag()->model2('file', $match['opponent']['image_id'])->path() ?>" class="img-fluid" alt="" />
 		</div>
 		<?php endif ?>
 		<div class="text-left col-xs-<?php echo $match['opponent']['image_id'] ? 4 : 5 ?> vcenter">
@@ -58,7 +58,7 @@
 			<div class="row vcenter">
 				<?php if ($rounds[$i]['image_id']): ?>
 				<div class="col-2 vcenter">
-					<img src="<?php echo path($rounds[$i]['image_id']) ?>" class="img-fluid" alt="" />
+					<img src="<?php echo NeoFrag()->model2('file', $rounds[$i]['image_id'])->path() ?>" class="img-fluid" alt="" />
 				</div>
 				<?php endif ?>
 				<div class="col-xs-<?php echo $rounds[$i]['image_id'] ? '3' : '5' ?> vcenter">

modules/gallery/views/index.tpl.php
@@ -1,5 +1,5 @@
 <?php if (!empty($category_image)): ?>
-<img class="img-fluid" src="<?php echo path($category_image) ?>" alt="" />
+<img class="img-fluid" src="<?php echo NeoFrag()->model2('file', $category_image)->path() ?>" alt="" />
 <?php endif ?>
 <div class="panel-body">
 	<?php if (!empty($gallery)): ?>
@@ -7,7 +7,7 @@
 		<?php foreach ($gallery as $album): ?>
 			<div class="col-12">
 				<h4><a href="<?php echo url('gallery/album/'.$album['gallery_id'].'/'.url_title($album['title'])) ?>"><?php echo $album['title'] ?></a></h4>
-				<a href="<?php echo url('gallery/album/'.$album['gallery_id'].'/'.url_title($album['title'])) ?>" class="gallery-item thumbnail" style="background-image: url('<?php echo path(!empty($album['image']) ? $album['image'] : $this->db->select('file_id')->from('nf_gallery_images')->where('gallery_id', $album['gallery_id'])->order_by('RAND()')->limit(1)->row()) ?>');">
+				<a href="<?php echo url('gallery/album/'.$album['gallery_id'].'/'.url_title($album['title'])) ?>" class="gallery-item thumbnail" style="background-image: url('<?php echo NeoFrag()->model2('file', !empty($album['image']) ? $album['image'] : $this->db->select('file_id')->from('nf_gallery_images')->where('gallery_id', $album['gallery_id'])->order_by('RAND()')->limit(1)->row())->path() ?>);">
 					<div class="black-caption-hover">
 						<div class="black-caption-hover-content">
 							<h4 class="m-0"><?php echo icon('fa-eye').' '.$this->lang('Ouvrir l\'album') ?></h4>

modules/teams/views/index.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($image_id): ?>
-<a href="<?php echo url('teams/'.$team_id.'/'.$name) ?>"><img class="img-fluid" src="<?php echo path($image_id) ?>" alt="" /></a>
+<a href="<?php echo url('teams/'.$team_id.'/'.$name) ?>"><img class="img-fluid" src="<?php echo NeoFrag()->model2('file', $image_id)->path() ?>" alt="" /></a>
 <?php endif ?>
 <div class="panel-body">
 	<?php if ($description): ?>

widgets/events/views/matches.tpl.php
@@ -8,7 +8,7 @@
 			</div>
 			<?php if ($match['match']['opponent']['image_id']): ?>
 			<div class="text-center col-1">
-				<img src="<?php echo path($match['match']['opponent']['image_id']) ?>" class="img-fluid" alt="" />
+				<img src="<?php echo NeoFrag()->model2('file', $match['match']['opponent']['image_id'])->path() ?>" class="img-fluid" alt="" />
 			</div>
 			<?php endif ?>
 			<div class="text-left col-<?php echo $match['match']['opponent']['image_id'] ? 7 : 8 ?>">

widgets/gallery/controllers/index.php
@@ -50,7 +50,7 @@ class Index extends Controller_Widget
 		{
 			return $this->panel()
 						->heading($image['title'])
-						->body('<a href="'.$href.'"><img class="img-fluid" src="'.path($image['file_id']).'" alt="" /></a>', FALSE)
+						->body('<a href="'.$href.'"><img class="img-fluid" src="'.NeoFrag()->model2('file', $image['file_id'])->path().'" alt="" /></a>', FALSE)
 						->footer('<a href="'.$href.'">'.icon('fa-arrow-circle-o-right').' '.$this->lang('Détails').'</a>', 'right');
 		}
 		else