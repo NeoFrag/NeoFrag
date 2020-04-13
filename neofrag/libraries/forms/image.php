c1de65a48270c36fc97f099f7defefde205e5790
Jérémy VALENTIN <eresnova@neofrag.fr>
2020-04-13 15:33:11 +0200

    [Library][Form2][Image] Add default() method

--- neofrag/libraries/forms/image.php
+++ neofrag/libraries/forms/image.php
@@ -15,6 +15,7 @@ class Image extends File
 	];
 	protected $_width;
 	protected $_height;
+	protected $_default;
 
 	public function __invoke($name, $upload_dir = '')
 	{
@@ -35,16 +36,25 @@ class Image extends File
 		};
 
 		$this->_thumbnail = function(){
-			if ($this->_value)
-			{
-				return '<div class="text-center">
-							<img class="img-thumbnail" src="'.$this->_value->path().'" alt="" />
-							<p class="m-4">'.$this->lang('Dimensions %dpx par %dpx <i>(%s max.)</i>', $this->_width, $this->_height, human_size(file_upload_max_size())).'</p>
-						</div>';
-			}
+			return $this->html()
+						->attr('class', 'text-center')
+						->append_if($path = ($this->_value ? $this->_value->path() : $this->_default), '<img class="img-thumbnail" src="'.$path.'" alt="" />')
+						->append_if($this->_width || $this->_height,                  '<p class="m-4">'.$this->lang('Dimensions %dpx par %dpx <i>(%s max.)</i>', $this->_width, $this->_height, human_size(file_upload_max_size())).'</p>');
 		};
 
 		return parent::__invoke($name, $upload_dir);
+	}
+
+	public function __call($name, $args)
+	{
+		//TODO 5.6 compatibility
+		if ($name == 'default')
+		{
+			$this->_default = $args[0];
+			return $this;
+		}
+
+		return parent::__call($name, $args);
 	}
 
 	public function square($size)
