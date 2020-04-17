ee170c46ba81673a20c4c430d4b696d0d4c4bd09
Jérémy VALENTIN <eresnova@neofrag.fr>
2020-04-17 15:42:49 +0200

    [Bootstrap] Some migrations

--- modules/search/views/index.tpl.php
+++ modules/search/views/index.tpl.php
@@ -4,8 +4,8 @@
 <?php endif ?>
 <form action="<?php echo url('search') ?>" method="get">
 	<div class="input-group">
-		<input type="text" class="form-control input-lg" name="q" value="<?php echo utf8_htmlentities($keywords) ?>" placeholder="<?php echo $this->lang('Rechercher un ou plusieurs termes sur notre site') ?>" />
-		<span class="input-group-append btn-group-lg">
+		<input type="text" class="form-control form-control-lg" name="q" value="<?php echo utf8_htmlentities($keywords) ?>" placeholder="<?php echo $this->lang('Rechercher un ou plusieurs termes sur notre site') ?>" />
+		<span class="input-group-append">
 			<button class="btn btn-primary btn-lg" type="submit"><?php echo icon('fas fa-search').' '.$this->lang('Rechercher') ?></button>
 		</span>
 	</div>
