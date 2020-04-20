8516b2de61b82a5e232eba6bfa11f21cc9c7698a
Michaël BILCOT <foxley@neofrag.fr>
2020-04-20 14:31:41 +0200

    [Library][MysqlDump] Some improvements with tables list, added skip_data() and skip_structure() methods

--- neofrag/libraries/mysqldump.php
+++ neofrag/libraries/mysqldump.php
@@ -19,20 +19,56 @@ use NF\NeoFrag\Library;
 class Mysqldump extends Library
 {
 	const MAX_SQL_SIZE = 50000;
+	const DATA         = 1;
+	const STRUCTURE    = 2;
+
+	protected $_skip_data;
+	protected $_skip_structure;
+	protected $_tables = [];
+
+	public function skip_data()
+	{
+		$this->_skip_data = TRUE;
+		return $this;
+	}
+
+	public function skip_structure()
+	{
+		$this->_skip_structure = TRUE;
+		return $this;
+	}
+
+	public function tables($tables)
+	{
+		if ($tables && !array_key_exists('data', $tables) && !array_key_exists('structure', $tables))
+		{
+			$tables = array_fill_keys(['data', 'structure'], $tables);
+		}
+
+		$this->_tables = $tables;
+
+		return $this;
+	}
 
 	public function dump($handle, $callback = NULL)
 	{
-		$this->db->lock($tables = $this->db->tables());
+		$tables = [];
 
-		if (is_callable($callback))
+		foreach ($this->db->tables() as $table)
 		{
-			$total = $i = 0;
+			$tables[$table] = 0;
 
-			foreach ($tables as $table)
+			foreach (['data', 'structure'] as $action)
 			{
-				$total += $this->db->from($table)->count();
+				if ((!$this->{'_skip_'.$action} && (empty($this->_tables[$action]) || !in_array($table, $this->_tables[$action]))) ||
+					($this->{'_skip_'.$action}  && !empty($this->_tables[$action]) && in_array($table, $this->_tables[$action])))
+				{
+					$tables[$table] |= constant('self::'.strtoupper($action));
+				}
 			}
 		}
+
+		$tables = array_filter($tables);
 
 		fwrite($handle, '-- NeoFrag '.NEOFRAG_VERSION.PHP_EOL.
 						'-- https://neofr.ag'.PHP_EOL.
@@ -49,88 +85,114 @@ class Mysqldump extends Library
 						'-- Database: '.self::_delimite($this->db->get_info('name')).PHP_EOL.
 						'--'.PHP_EOL);
 
-		foreach ($tables as $table)
+		if ($tables)
 		{
-			fwrite($handle, PHP_EOL.
-							'-- --------------------------------------------------------'.PHP_EOL.PHP_EOL.
-							'--'.PHP_EOL.
-							'-- Table structure for table '.self::_delimite($table).PHP_EOL.
-							'--'.PHP_EOL.PHP_EOL.
-							'DROP TABLE IF EXISTS '.self::_delimite($table).';'.PHP_EOL.
-							$this->db->table_create($table).';'.PHP_EOL);
+			$this->db->lock($tables);
 
-			$cols = $this->db->table_columns($table);
-
-			$size = $dump = 0;
-
-			$res = $this->db->query('SELECT * FROM '.self::_delimite($table))->results();
-			while ($row = $this->db->fetch($res))
+			if (is_callable($callback))
 			{
-				if (!$dump)
+				$total = $i = 0;
+
+				foreach ($tables as $table => $action)
 				{
-					$dump = TRUE;
+					if ($action & self::DATA)
+					{
+						$total += $this->db->from($table)->count();
+					}
+				}
+			}
+
+			foreach ($tables as $table => $action)
+			{
+				if ($action & self::STRUCTURE)
+				{
+					$structure = $this->db->table_create($table);
 
 					fwrite($handle, PHP_EOL.
+									'-- --------------------------------------------------------'.PHP_EOL.PHP_EOL.
 									'--'.PHP_EOL.
-									'-- Dumping data for table '.self::_delimite($table).PHP_EOL.
-									'--'.PHP_EOL);
+									'-- Table structure for table '.self::_delimite($table).PHP_EOL.
+									'--'.PHP_EOL.PHP_EOL.
+									'DROP TABLE IF EXISTS '.self::_delimite($table).';'.PHP_EOL.
+									$structure.';'.PHP_EOL);
 				}
 
-				if ($size == 0)
+				if ($action & self::DATA)
 				{
-					fwrite($handle, PHP_EOL.'INSERT INTO '.self::_delimite($table).' ('.implode(', ', array_map([$this, '_delimite'], array_keys($cols))).') VALUES'.PHP_EOL);
-				}
-				else
-				{
-					fwrite($handle, ','.PHP_EOL);
-				}
+					$cols = $this->db->table_columns($table);
 
-				$values = [];
+					$size = $dump = 0;
 
-				foreach ($row as $key => $value)
-				{
-					if ($value === NULL)
+					$res = $this->db->query('SELECT * FROM '.self::_delimite($table))->results();
+					while ($row = $this->db->fetch($res))
 					{
-						$values[] = 'NULL';
+						if (!$dump)
+						{
+							$dump = TRUE;
+
+							fwrite($handle, PHP_EOL.
+											'--'.PHP_EOL.
+											'-- Dumping data for table '.self::_delimite($table).PHP_EOL.
+											'--'.PHP_EOL);
+						}
+
+						if ($size == 0)
+						{
+							fwrite($handle, PHP_EOL.'INSERT INTO '.self::_delimite($table).' ('.implode(', ', array_map([$this, '_delimite'], array_keys($cols))).') VALUES'.PHP_EOL);
+						}
+						else
+						{
+							fwrite($handle, ','.PHP_EOL);
+						}
+
+						$values = [];
+
+						foreach ($row as $key => $value)
+						{
+							if ($value === NULL)
+							{
+								$values[] = 'NULL';
+							}
+							elseif (preg_match('#^[^(]*(BYTE|COUNTER|SERIAL|INT|LONG$|CURRENCY|REAL|MONEY|FLOAT|DOUBLE|DECIMAL|NUMERIC|NUMBER)#i', $cols[$key]))
+							{
+								$values[] = str_replace(',', '.', $value);
+							}
+							else
+							{
+								$values[] = '\''.static::_utf8($this->db->escape_string($value)).'\'';
+							}
+						}
+
+						fwrite($handle, $line = '('.implode(', ', $values).')');
+
+						$size += strlen($line);
+						if ($size > self::MAX_SQL_SIZE)
+						{
+							fwrite($handle, ';'.PHP_EOL);
+							$size = 0;
+						}
+
+						if (is_callable($callback))
+						{
+							$callback(++$i / $total * 100);
+						}
 					}
-					elseif (preg_match('#^[^(]*(BYTE|COUNTER|SERIAL|INT|LONG$|CURRENCY|REAL|MONEY|FLOAT|DOUBLE|DECIMAL|NUMERIC|NUMBER)#i', $cols[$key]))
+
+					$this->db->free($res);
+
+					if ($size)
 					{
-						$values[] = str_replace(',', '.', $value);
+						fwrite($handle, ';'.PHP_EOL);
 					}
-					else
-					{
-						$values[] = '\''.static::_utf8($this->db->escape_string($value)).'\'';
-					}
-				}
-
-				fwrite($handle, $line = '('.implode(', ', $values).')');
-
-				$size += strlen($line);
-				if ($size > self::MAX_SQL_SIZE)
-				{
-					fwrite($handle, ';'.PHP_EOL);
-					$size = 0;
-				}
-
-				if (is_callable($callback))
-				{
-					$callback(++$i / $total * 100);
 				}
 			}
 
-			$this->db->free($res);
-
-			if ($size)
-			{
-				fwrite($handle, ';'.PHP_EOL);
-			}
+			$this->db->unlock($tables);
 		}
 
 		fwrite($handle, PHP_EOL.'-- --------------------------------------------------------'.PHP_EOL.PHP_EOL.'SET FOREIGN_KEY_CHECKS = 1;'.PHP_EOL);
 
 		fclose($handle);
-
-		$this->db->unlock($tables);
 	}
 
 	static private function _delimite($s)
